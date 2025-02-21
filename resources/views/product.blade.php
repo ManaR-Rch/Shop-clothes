dafe9398e29dbe4679040751748eeddad9f0abb8
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-02-21 16:52:47 +0100

    feat: Ajouter le view 'product'

---
+++ resources/views/product.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>product</title>
+</head>
+
+<body>
+    <main class="grid grid-col-4 gap-2">
+        <article class="bg-red-400">
+            <div>card1</div>
+        </article>
+        <article class="bg-red-400">
+            <div>card2</div>
+        </article>
+        <article class="bg-red-400">
+            <div>card3</div>
+        </article>
+        <article class="bg-red-400">
+            <div>card4</div>
+        </article>
+    </main>
+
+</body>
+
+</html>
